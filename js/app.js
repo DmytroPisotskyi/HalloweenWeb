5d7431bd0276eb2c7d1b9d204de218ea11177b3f
Dmytro Pisotskyi <pisotskyidmytro12@gmail.com>
2024-10-29 23:31:47 +0200

    footer 1.1 main 1.2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
-    const footer = document.querySelector('.footer');
-
+    
     if ( window.scrollY > 10 ) {
         header.classList.add('scroll');
-        footer.classList.add('active-footer');
     } else {
         header.classList.remove('scroll');
-        footer.classList.remove('active-footer');
     }
 });
 
